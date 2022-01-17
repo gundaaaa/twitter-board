1a4a69b11e0d536f4786b69b9385d246de33b55d
gundaaaa <83895029+gundaaaa@users.noreply.github.com>
2022-01-17 12:15:19 +0900

    変更

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 // 日本時間にする
 date_default_timezone_set('Asia/Tokyo');
 // URL/ディレクトリ設定
-define('HOME_URL', '/twitter%20board/');
+define('HOME_URL', '/twitterboard/');
 
 
 ///////////////////////////////////////
